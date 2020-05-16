3352e62ed7c0cad172fd1292b85572392efa11f5
Uma Lubell <Uma Lubell@DESKTOP-O85TMHK>
2020-05-16 15:51:45 -0400

    fixed errors

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -6,15 +6,14 @@ public class Demo {
 		System.out.println("Group One");
 //		adding the new line of codes to the class.
 		System.out.println("Group one is initially online Students");
-		
-	}
-	int num1 = 20;
-	int num2 = 34;
-	if (num1 == num2) {
-		System.out.println("They are equal");
-	} else {
-		System.out.println("adjustment needed");
-	}
+
+		int num1 = 20;
+		int num2 = 34;
+		if (num1 == num2) {
+			System.out.println("They are equal");
+		} else {
+			System.out.println("adjustment needed");
+		}
 
 	}
 
