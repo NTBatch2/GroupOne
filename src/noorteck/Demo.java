713b93b4fe5ef772ae5b390de2961c8157decfd5
abdulwali <abdulwali@abduls-mbp.hsd1.tx.comcast.net>
2020-05-16 14:24:30 -0500

    Added the new line of code

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -4,5 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Group One");
+//		adding the new line of codes to the class.
+		System.out.println("Group one is initially online Students");
+		
 	}
 }
