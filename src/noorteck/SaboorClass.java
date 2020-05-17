57e6872c89a16c856408ec1001c90c0d1e1e7345
Abdul Azizi <saboorazizi.82@gmail.com>
2020-05-17 09:36:30 -0700

    What is next?

--- src/noorteck/SaboorClass.java
+++ src/noorteck/SaboorClass.java
@@ -14,5 +14,8 @@ public class SaboorClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("The New Class for Group 1 !");
+		
+		// What is Next?
+		System.out.println("The New Class for Group 1 !");
 	}
 }
