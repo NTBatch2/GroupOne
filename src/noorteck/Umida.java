702dcc9be1b49a7b4c42596f3557c520c531d736
abdulwali <abdulwali@abduls-mbp.hsd1.tx.comcast.net>
2020-05-17 11:40:00 -0500

    added new code

--- src/noorteck/Umida.java
+++ src/noorteck/Umida.java
@@ -5,6 +5,7 @@ public class Umida {
 	public static void main(String[] args) {
 		System.out.println("Hope everybody is happy with GitHub");
 		System.out.println("Great Job Umida");
+		System.out.println("Who else want to push new codes");
 	}
 	
 	public void testData() {
