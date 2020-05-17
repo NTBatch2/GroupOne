d40118a299b1ec41573cdf6fbb3c66a494b08c72
andreamejia <andreamejia@localhost>
2020-05-17 10:05:12 -0500

    changes

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -6,16 +6,15 @@ public class Demo {
 		System.out.println("Group One");
 //		adding the new line of codes to the class.
 		System.out.println("Group one is initially online Students");
-
-		int num1 = 20;
-		int num2 = 34;
-		if (num1 == num2) {
-			System.out.println("They are equal");
-		} else {
-			System.out.println("adjustment needed");
-		}
-//		adding my last codes for today 
-		System.out.println("The last codes for today, see you all tomorrow");
+		
+	}
+	int num1 = 20;
+	int num2 = 34;
+	if (num1 == num2) {
+		System.out.println("They are equal");
+	} else {
+		System.out.println("adjustment needed");
+	}
 
 	}
 
@@ -24,19 +23,9 @@ public class Demo {
 		System.out.println("hello from New York");
 		System.out.println("When I try to push it gives me error");
 
-		// umida second time adding
-//		Added the new line of code:
-		System.out.println("Good Morning EveryOne");
-		System.out.println("Good Morning EveryOne, this section needs group practice lets be on the top of it");
-		
+		//umida second time adding
 	}
 
 	//Good morning Group 1. Have a successful day in GitHub!
 	
-	//concrete method 
-	// hi
-	public void write() {
-		System.out.println("This is Umida");
-		System.out.println("Why only Abdul and I are pushing codes only?");
-	}
-}
+
